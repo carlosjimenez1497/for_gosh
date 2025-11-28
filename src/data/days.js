e540fee929ed141b1c7a2888f3cead77d80424d6
CarlosJimenez <163073359+carlosjimenez1497@users.noreply.github.com>
2025-11-28 16:46:08 +0100

    Update days.js

--- src/data/days.js
+++ src/data/days.js
@@ -9,7 +9,12 @@ export const days = [
     ghibliImage: "/images/day1-ghibli.png",
     image: "/images/day1.jpg",
 
-    message: "Just one word, but it’s ours ❤️",
+    message: "Woohoo yeah, correct, toh correct only
+Your birthday is close, and I want to wish you a happy birthday from now, I want to celebrate with this message the good memories I have with you, like this in Marken-Voli.
+
+I hope you have a nice rest of the day
+
+I toh love you only",
     coupon: "1 warm hug redeemable anytime 🤗"
   },
   {
